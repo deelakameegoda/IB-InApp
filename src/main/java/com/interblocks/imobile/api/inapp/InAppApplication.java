835f47f0faa5e2d1ef323a93cfc1ec09e7cd0fe4
deelakameegoda <deelakameegoda@gmail.com>
2020-03-24 22:37:49 +0530

    Initial Commit

---
+++ src/main/java/com/interblocks/imobile/api/inapp/InAppApplication.java
@@ -0,0 +1,22 @@
+package com.interblocks.imobile.api.inapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class InAppApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(InAppApplication.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(InAppApplication.class, args);
+    }
+
+}
